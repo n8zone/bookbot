47ad9537b65ad74d2e90c1ac4a111f657d9a4adc
Nathan <92413483+n8zone@users.noreply.github.com>
2024-08-31 07:17:20 -0400

    Added char count

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+def count_character(text):
+	text = text.lower()
+	text_array = text.split()
+	char_count_dict = {}
+	for w in text_array:
+		for c in w:
+			if c not in char_count_dict:
+				char_count_dict[c] = 0
+			char_count_dict[c] += 1
+
+	return char_count_dict
+
+
 def count_words(text):
 	text_array = text.split()
 	return len(text_array)
@@ -6,6 +19,8 @@ def main():
 	with open('books/frankenstein.txt') as f:
 		file_contents = f.read()
 		print(count_words(file_contents))
+
+		print(count_character(file_contents))
 
 
 
